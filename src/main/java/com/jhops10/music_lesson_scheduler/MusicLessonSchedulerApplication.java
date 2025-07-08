f069aadb25c2bb4a5eb57f03b21d007ebcac0a08
João Paulo <joaowais@gmail.com>
2025-07-08 18:21:31 -0300

    first commit

---
+++ src/main/java/com/jhops10/music_lesson_scheduler/MusicLessonSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.jhops10.music_lesson_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicLessonSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicLessonSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhops10/music_lesson_scheduler/MusicLessonSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhops10.music_lesson_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicLessonSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
