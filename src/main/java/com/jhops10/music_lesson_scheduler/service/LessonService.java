17681b1fc91e368a020b6dfe98d6e08346377abd
João Paulo <joaowais@gmail.com>
2025-07-10 16:28:53 -0300

    feat: cria LessonService com método create (POST)

---
+++ src/main/java/com/jhops10/music_lesson_scheduler/service/LessonService.java
@@ -0,0 +1,33 @@
+package com.jhops10.music_lesson_scheduler.service;
+
+import com.jhops10.music_lesson_scheduler.dto.lesson.LessonRequestDTO;
+import com.jhops10.music_lesson_scheduler.dto.lesson.LessonResponseDTO;
+import com.jhops10.music_lesson_scheduler.exceptions.StudentNotFoundException;
+import com.jhops10.music_lesson_scheduler.model.Lesson;
+import com.jhops10.music_lesson_scheduler.model.Student;
+import com.jhops10.music_lesson_scheduler.repository.LessonRepository;
+import com.jhops10.music_lesson_scheduler.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LessonService {
+
+    private final LessonRepository lessonRepository;
+    private final StudentRepository studentRepository;
+
+    public LessonResponseDTO create(LessonRequestDTO dto) {
+        Student student = studentRepository.findById(dto.studentId())
+                .orElseThrow(() -> new StudentNotFoundException("Aluno com o id " + dto.studentId() + " não encontrado."));
+
+        Lesson lesson = Lesson.builder()
+                .startTime(dto.startTime())
+                .student(student)
+                .notifyBeforeMinutes(dto.notifyBeforeMinutes())
+                .build();
+
+        Lesson saved = lessonRepository.save(lesson);
+        return LessonResponseDTO.fromEntity(saved);
+    }
+}
