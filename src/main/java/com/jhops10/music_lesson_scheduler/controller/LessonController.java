117a02000844d1cd6c7647b0d0f5f07a766abaf0
João Paulo <joaowais@gmail.com>
2025-07-10 16:29:26 -0300

    feat: implementa LessonController com endpoint de criação (POST)

---
+++ src/main/java/com/jhops10/music_lesson_scheduler/controller/LessonController.java
@@ -0,0 +1,27 @@
+package com.jhops10.music_lesson_scheduler.controller;
+
+import com.jhops10.music_lesson_scheduler.dto.lesson.LessonRequestDTO;
+import com.jhops10.music_lesson_scheduler.dto.lesson.LessonResponseDTO;
+import com.jhops10.music_lesson_scheduler.service.LessonService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/lessons")
+@RequiredArgsConstructor
+public class LessonController {
+
+    private final LessonService lessonService;
+
+    @PostMapping
+    public ResponseEntity<LessonResponseDTO> createLesson(@RequestBody @Valid LessonRequestDTO dto) {
+        LessonResponseDTO created = lessonService.create(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+}
