a7f8e18460c114fecc5b5ad4d37a4e682ef0eaf6
João Paulo <joaowais@gmail.com>
2025-07-14 21:42:31 -0300

    feat: adiciona endpoint de listagem (GET) no StudentController

--- src/main/java/com/jhops10/music_lesson_scheduler/controller/StudentController.java
+++ src/main/java/com/jhops10/music_lesson_scheduler/controller/StudentController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
@@ -23,5 +22,11 @@ public class StudentController {
     public ResponseEntity<StudentResponseDTO> createStudent(@RequestBody @Valid StudentRequestDTO dto) {
         StudentResponseDTO created = studentService.create(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StudentResponseDTO>> getAllStudents() {
+        List<StudentResponseDTO> students = studentService.getAll();
+        return ResponseEntity.ok().body(students);
     }
 }
