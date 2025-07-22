eb2f3a63d164a24d99d34918aedeb4171b26c8e4
João Paulo <joaowais@gmail.com>
2025-07-22 16:50:16 -0300

    feat(LessonNotificationLogRepository): métodos de busca findAllByOrderByNotifiedAtDesc e findByDeliveryMethodIgnoreCase

--- src/main/java/com/jhops10/music_lesson_scheduler/repository/LessonNotificationLogRepository.java
+++ src/main/java/com/jhops10/music_lesson_scheduler/repository/LessonNotificationLogRepository.java
@@ -4,6 +4,10 @@ import com.jhops10.music_lesson_scheduler.model.LessonNotificationLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LessonNotificationLogRepository extends JpaRepository<LessonNotificationLog, Long> {
+    List<LessonNotificationLog> findAllByOrderByNotifiedAtDesc();
+    List<LessonNotificationLog> findByDeliveryMethodIgnoreCase(String deliveryMethod);
 }
