8a6da93c3cdcf21060a7525d42cd7911228d61d5
João Paulo <joaowais@gmail.com>
2025-07-30 20:40:41 -0300

    test(StudentService): adiciona testes para método update (cenários de sucesso e falha)

--- src/test/java/com/jhops10/music_lesson_scheduler/service/StudentServiceTest.java
+++ src/test/java/com/jhops10/music_lesson_scheduler/service/StudentServiceTest.java
@@ -2,6 +2,7 @@ package com.jhops10.music_lesson_scheduler.service;
 
 import com.jhops10.music_lesson_scheduler.dto.student.StudentRequestDTO;
 import com.jhops10.music_lesson_scheduler.dto.student.StudentResponseDTO;
+import com.jhops10.music_lesson_scheduler.dto.student.StudentUpdateDTO;
 import com.jhops10.music_lesson_scheduler.exceptions.StudentNotFoundException;
 import com.jhops10.music_lesson_scheduler.model.Lesson;
 import com.jhops10.music_lesson_scheduler.model.Student;
@@ -31,12 +32,14 @@ class StudentServiceTest {
     private StudentRepository studentRepository;
 
 
+    private final Long nonExistingId = 9999999L;
+    private final Long defaultId = 1L;
     private Student defaultStudent;
     private List<Lesson> lessons = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
-        defaultStudent = createNewStudent(1L, "Name Example", "Example Instrument Name", lessons);
+        defaultStudent = createNewStudent(defaultId, "Name Example", "Example Instrument Name", lessons);
     }
 
     private Student createNewStudent(Long id, String studentName, String instrument, List<Lesson> lessons) {
@@ -96,28 +99,61 @@ class StudentServiceTest {
 
     @Test
     void getStudentById_shouldReturnStudent_whenIdExists() {
-        when(studentRepository.findById(1L)).thenReturn(Optional.of(defaultStudent));
+        when(studentRepository.findById(defaultId)).thenReturn(Optional.of(defaultStudent));
 
-        StudentResponseDTO sut = studentService.getById(1L);
+        StudentResponseDTO sut = studentService.getById(defaultId);
 
         assertNotNull(sut);
-        assertEquals(1, sut.id());
+        assertEquals(defaultId, sut.id());
         assertEquals("Name Example", sut.studentName());
         assertEquals("Example Instrument Name", sut.instrument());
 
-        verify(studentRepository).findById(1L);
+        verify(studentRepository).findById(defaultId);
         verifyNoMoreInteractions(studentRepository);
     }
 
     @Test
     void getStudentById_shouldThrowException_whenIdDoesNotExist() {
-        when(studentRepository.findById(9999999L)).thenReturn(Optional.empty());
+        when(studentRepository.findById(nonExistingId)).thenReturn(Optional.empty());
 
-        assertThrows(StudentNotFoundException.class, () -> studentService.getById(9999999L));
+        assertThrows(StudentNotFoundException.class, () -> studentService.getById(nonExistingId));
 
-        verify(studentRepository).findById(9999999L);
+        verify(studentRepository).findById(nonExistingId);
         verifyNoMoreInteractions(studentRepository);
 
+
+    }
+
+    @Test
+    void updateStudent_shouldReturnUpdatedStudent_whenIdExists() {
+        StudentUpdateDTO updateDTO = new StudentUpdateDTO("Updated Name", "Updated Instrument Name");
+        Student updatedStudent = createNewStudent(defaultStudent.getId(), updateDTO.studentName(), updateDTO.instrument(), defaultStudent.getLessons());
+
+        when(studentRepository.findById(defaultId)).thenReturn(Optional.of(defaultStudent));
+        when(studentRepository.save(any(Student.class))).thenReturn(updatedStudent);
+
+        StudentResponseDTO sut = studentService.update(defaultStudent.getId(), updateDTO);
+
+        assertNotNull(sut);
+        assertEquals(updatedStudent.getId(), sut.id());
+        assertEquals(updatedStudent.getStudentName(), sut.studentName());
+        assertEquals(updatedStudent.getInstrument(), sut.instrument());
+
+        verify(studentRepository).findById(defaultId);
+        verify(studentRepository).save(defaultStudent);
+        verifyNoMoreInteractions(studentRepository);
+    }
+
+    @Test
+    void updateStudent_shouldThrowException_whenIdDoesNotExist() {
+        StudentUpdateDTO updateDTO = new StudentUpdateDTO("Updated Name", "Updated Instrument Name");
+
+        when(studentRepository.findById(nonExistingId)).thenReturn(Optional.empty());
+
+        assertThrows(StudentNotFoundException.class, () -> studentService.update(nonExistingId, updateDTO));
+
+        verify(studentRepository).findById(nonExistingId);
+        verifyNoMoreInteractions(studentRepository);
 
     }
 
